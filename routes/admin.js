f37cfcb48c1ff0e872dd3a98f16ac254cb80f938
Steve Sunny <stevesunny99@gmail.com>
2023-12-07 15:21:04 +1000

    site setup with just adding on item .add routes in admin and product.add new product controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import * as productsController from '../controllers/products.js';
 
 const router = express();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 router.set('view engine', 'ejs');
 router.set('views', './views');
+
+// Admin routes
+
+
+router.get('/add-product', productsController.getAddProduct);
+router.post('/add-product',productsController.postAddProduct);
+
 
 
 export { router};
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import * as productsController from '../controllers/products.js';
 const router = express();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
@@ -9,5 +9,6 @@ router.set('view engine', 'ejs');
 router.set('views', './views');
 
 
+router.get("/",productsController.getProducts);
 
 export { router };
